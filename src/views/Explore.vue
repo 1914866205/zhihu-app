94f649c733e05d46b3be55c9179b64d7c4fe000e
事在人为 <1914866205@qq.com>
2020-01-20 22:42:51 +0800

    收藏

--- src/views/Explore.vue
+++ src/views/Explore.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <svg class="Zi Zi--LabelSpecial" style="padding-left:75px;color:#0084ff;}" fill="currentColor" viewBox="0 0 24 24" width="36" height="36"><path d="M7.667 3.667h11.466a1.2 1.2 0 0 1 1.2 1.2v13.066a2.4 2.4 0 0 1-2.4 2.4H6.467V4.867a1.2 1.2 0 0 1 1.2-1.2zM4.2 9.619h1.689v10.714H5.4a2.4 2.4 0 0 1-2.4-2.4V10.82a1.2 1.2 0 0 1 1.2-1.2zm5.178-2.38a.6.6 0 0 0-.6.6v.585a.6.6 0 0 0 .6.6h8.044a.6.6 0 0 0 .6-.6v-.586a.6.6 0 0 0-.6-.6H9.378zm0 3.57a.6.6 0 0 0-.6.6v.586a.6.6 0 0 0 .6.6h8.044a.6.6 0 0 0 .6-.6v-.585a.6.6 0 0 0-.6-.6H9.378zm0 3.572a.6.6 0 0 0-.6.6v.586a.6.6 0 0 0 .6.6h4.578a.6.6 0 0 0 .6-.6v-.586a.6.6 0 0 0-.6-.6H9.378z" fill-rule="evenodd"></path></svg>
+        <svg  style="padding-left:75px;color:#0084ff;}" fill="currentColor" viewBox="0 0 24 24" width="36" height="36"><path d="M7.667 3.667h11.466a1.2 1.2 0 0 1 1.2 1.2v13.066a2.4 2.4 0 0 1-2.4 2.4H6.467V4.867a1.2 1.2 0 0 1 1.2-1.2zM4.2 9.619h1.689v10.714H5.4a2.4 2.4 0 0 1-2.4-2.4V10.82a1.2 1.2 0 0 1 1.2-1.2zm5.178-2.38a.6.6 0 0 0-.6.6v.585a.6.6 0 0 0 .6.6h8.044a.6.6 0 0 0 .6-.6v-.586a.6.6 0 0 0-.6-.6H9.378zm0 3.57a.6.6 0 0 0-.6.6v.586a.6.6 0 0 0 .6.6h8.044a.6.6 0 0 0 .6-.6v-.585a.6.6 0 0 0-.6-.6H9.378zm0 3.572a.6.6 0 0 0-.6.6v.586a.6.6 0 0 0 .6.6h4.578a.6.6 0 0 0 .6-.6v-.586a.6.6 0 0 0-.6-.6H9.378z" fill-rule="evenodd"></path></svg>
         <span style="margin-left: 12px;font-size: 25px;font-weight: 600;font-synthesis: style;">最新专题</span>
         <div class="banner">
             <div class="container">
@@ -29,9 +29,9 @@
         <div class="container">
              <div v-for="(item,index) in recommoned" :key="index">
             <div class="body">
-             <div :style="{backgroundImage:'url('+item.banner+')'}" style="width:98%;height:200px;background-repeat: no-repeat;background-attachment: scroll;background-position:-140px -20px;">
+             <div :style="{backgroundImage:'url('+item.banner+')'}" style="width:700px;height:200px;background-repeat: no-repeat;background-attachment: scroll;background-position:-140px -20px;">
+                <button class="guanzhu2">关注圆桌</button>
                 <h3 class="title2">{{ item.title}}</h3>
-                <button class="guanzhu">关注圆桌</button>
                 <div class="neirong2">
                     <small>{{item.introduction}}</small>
                 </div>
@@ -45,6 +45,27 @@
                 </button>
             </div>
         </div>
+        <div>
+            <div style="padding-left:75px"><svg fill="currentColor" viewBox="0 0 24 24" width="36" height="36" style="color: #0084ff">
+                <path d="M5.515 19.64l.918-5.355-3.89-3.792c-.926-.902-.639-1.784.64-1.97L8.56 7.74l2.404-4.871c.572-1.16 1.5-1.16 2.072 0L15.44 7.74l5.377.782c1.28.186 1.566 1.068.64 1.97l-3.89 3.793.918 5.354c.219 1.274-.532 1.82-1.676 1.218L12 18.33l-4.808 2.528c-1.145.602-1.896.056-1.677-1.218z" fill-rule="evenodd"></path>
+            </svg>
+                <span style="margin-left: 12px;font-size: 25px;font-weight: 600;font-synthesis: style;">热门收藏夹</span>
+               </div>
+            <div class="container">
+                <div v-for="(item,index) in favorite" :key="index">
+                    <div class="body">
+                        <button class="guanzhu">关注收藏夹</button>
+                        <h3 class="title">{{ item.title}}</h3>
+                        <div class="neirong">
+                            <small>{{item.introduction}}</small>
+                        </div>
+                        <p class="fangwen">{{ item.viewCount }}次浏览 {{item.followersCount}}关注</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
     </div>
 </template>
 
@@ -53,12 +74,15 @@
         name: "recommoned",
         data(){
             return{
-                recommoned:[]
+                recommoned:[],
+                favorite:[]
             };
         },created() {
-            this.axios.get('http://localhost:8080/api/special').then(res=>{
-                this.recommoned=res.data.data;
-            })
+            this.axios.get('http://localhost:8080/api/special').then(
+                res=>{this.recommoned=res.data.data;})
+        },createdfa() {
+            this.axios.get('http://localhost:8080/api/favorite').then(
+                res=>{this.favorite=res.data.data;})
         }
     }
 </script>
@@ -95,25 +119,34 @@
         color: white;
     }
     .guanzhu{
-        margin-left: 500px;
-        padding: 0;
-        font: inherit;
+        width: 120px;
         height: 34px;
-        color:#0084ff;
         background-color: rgba(0,132,255,.08);
-        width: 88px;
+        border: solid 0 lightgray;
+        color: #0084ff;
+        font: inherit;
         border-radius: 3px;
         font-size: 14px;
         font-weight: 600;
         font-synthesis: style;
-        cursor: pointer;
-        background: none;
-        background-color: rgba(0, 0, 0, 0);
-        border: none;
-        outline: none;
-        -webkit-appearance: none;
-        -moz-appearance: none;
+        float: right;
     }
+    .guanzhu2{
+        width: 80px;
+        height: 34px;
+        background-color: rgba(0,132,255,.08);
+        border: solid 0 lightgray;
+        color: #0084ff;
+        font: inherit;
+        border-radius: 3px;
+        font-size: 14px;
+        font-weight: 600;
+        font-synthesis: style;
+        margin-left: 585px;
+    }
+
+
+
 
     .image{
         margin-left: 20px;
